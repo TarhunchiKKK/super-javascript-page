bf85c6f67653935dfc5dbcccd9605524834bc823
TarhunchicKKK <TarhunchicKKK@yandex.ru>
2023-08-20 15:10:04 +0300

    Create gulp build

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+
+const paths = {
+    html:{
+        src: "./src/index.html",
+        dest: "./dist/"
+    },
+    css:{
+        src:["./src/**/*.scss", "./src/**/*.sass", "./src/**/*.css"],
+        dest: "./dist/css"
+    },
+    img:{
+        src: "./src/img/*",
+        dest: "./dist/img/"
+    }
+}
+
+function clean(){
+    return del(["./dist/*", "!dist/img", "!dist/bootstrap"]);
+}
+
+function html(){
+    return gulp.src(paths.html.src)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(paths.html.dest));
+}
+
+function css(){
+    return gulp.src(paths.css.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({cascade: false}))
+        .pipe(concat("styles.min.css"))
+        .pipe(cleanCSS({level: 2}))
+        .pipe(gulp.dest(paths.css.dest));
+}
+
+function img(){
+    return gulp.src(paths.img.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.img.dest));
+}
+
+exports.html = html;
+exports.css = css;
+exports.img = img;
+exports.clean = clean;
+exports.default = gulp.parallel(clean, html, css);
